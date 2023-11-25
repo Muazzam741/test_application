d00ed8e17ba8d52b8d465a79f9f3131b72945833
Ali Muazzam <92162554+Muazzam741@users.noreply.github.com>
2023-11-25 23:01:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import boto3
 app = FastAPI()
 aws_region = 'ap-south-1'
 table_name = 'QueueStatus'
-aws_access_key_id = 'AKIAYKIEN5RPYK6CAJPO'
-aws_secret_access_key = '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'
+aws_access_key_id = ''
+aws_secret_access_key = ''
 
 dynamodb = boto3.resource('dynamodb', region_name=aws_region, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 table = dynamodb.Table(table_name)
